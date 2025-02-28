4f4d7f78bd8bbb0e7e203eec9afd6bc4b704269a
Long-Soeng168 <generalpurpose167@gmail.com>
2025-02-28 17:23:31 +0700

    update

--- app/Livewire/SaleTableData.php
+++ app/Livewire/SaleTableData.php
@@ -31,7 +31,7 @@ class SaleTableData extends Component
     public $end_date = null;
     public function mount()
     {
-        $this->end_date = Carbon::today()->toDateString();
+        $this->end_date = Carbon::tomorrow()->toDateString();
     }
 
     public function setFilter($value)
